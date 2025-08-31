b454b00302d2a0e0d2f1c0acec45c016f4d63f09
elvinabinoy <elvinabinoy@gmail.com>
2025-08-31 19:35:30 +0530

    check

---
+++ app/src/main/res/xml/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.fireemergency
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This line links the logic to the layout file you just created.
+        setContentView(R.layout.activity_main)
+
+        // Find the SOS button by its ID from the XML layout.
+        val sosButton: Button = findViewById(R.id.btn_sos)
+
+        // Set an OnClickListener to the SOS button to handle clicks.
+        sosButton.setOnClickListener {
+            // This is where you would add the logic to alert emergency services.
+            // For now, it will display a temporary message.
+            Toast.makeText(this, "Emergency Alert Sent!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
